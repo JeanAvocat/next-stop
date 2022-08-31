8a30d700e2c0af6359d4fbe318a620a0c1a6212c
Alfred Gaudin <gaudin.all@gmail.com>
2022-08-31 14:09:21 +0200

    ok

--- app/controllers/trip_sessions_controller.rb
+++ app/controllers/trip_sessions_controller.rb
@@ -1,4 +1,41 @@
 class TripSessionsController < ApplicationController
   def new
+    if @tripsession != nil
+      @tripsession = TripSession.all
+      countpeople
+    else
+      @numberconnected = 0
+      @tripsession = []
+    end
+  end
+
+  def create
+    @tripsession = TripSession.new
+    @tripsession.status = "waiting for joiner"
+    @tripsession.creator = current_user
+    @tripsession.save
+    redirect_to trip_session_path(@tripsession)
+  end
+
+  def join
+    @tripsessions = TripSession.all
+    @tripsession = @tripsessions.last
+    @tripsession.joiner = current_user
+    @tripsession.status = "waiting for joiner"
+    redirect_to trip_session_path(@tripsession)
+  end
+
+  private
+
+  # méthode pour compter le nb de personnes connectés
+  def countpeople
+    @numberconnected = 0
+    @tripsession.each do |session|
+      if (session.creator != []) && (session.joiner != [])
+        @numberconnected += 2
+      else
+        @numberconnected += 1
+      end
+    end
   end
 end
