77c64aa85107b7d865fad2aa1ff356dfe0c954a3
benoit brandt <benoit.07@hotmail.com>
2022-08-31 16:17:37 +0200

    add logic to display cross or circle on tic tac toe box according to this id

--- app/controllers/game_matches_controller.rb
+++ app/controllers/game_matches_controller.rb
@@ -1,4 +1,6 @@
 class GameMatchesController < ApplicationController
   def show
+    @game_match = GameMatch.find(params[:id])
+    @tic_tac_toe_game = @game_match.matchable
   end
 end
--- app/controllers/tic_tac_toe_games_controller.rb
+++ app/controllers/tic_tac_toe_games_controller.rb
@@ -1,2 +1,19 @@
 class TicTacToeGamesController < ApplicationController
+  def select_tile
+    @game_match = GameMatch.find(params[:id])
+    @tic_tac_toe_game = @game_match.matchable
+    if @tic_tac_toe_game.cross_player == current_user
+      @tic_tac_toe_game.update("#{params[:tile]}": "X")
+    else
+      @tic_tac_toe_game.update("#{params[:tile]}": "O")
+    end
+
+    redirect_to game_match_path
+  end
+
+  private
+
+  def tic_tac_toe_game_params
+    params.require(:tile).permit(:tile)
+  end
 end
