d80e0d3c8106ffdc5856311ec04bb7585cca113e
benoit brandt <benoit.07@hotmail.com>
2022-08-31 20:07:38 +0200

    add operation to check if there is a winner after each update + remove possibility to override selection if the game is finish or a tile is already select

--- app/controllers/tic_tac_toe_games_controller.rb
+++ app/controllers/tic_tac_toe_games_controller.rb
@@ -8,18 +8,7 @@ class TicTacToeGamesController < ApplicationController
       @tic_tac_toe_game.update("#{params[:tile]}": "O")
     end
 
+    @tic_tac_toe_game.end_of_a_game?
     redirect_to game_match_path
   end
-
-  private
-
 end
-
-# winning_pattern = "1.2.3"
-# winning_pattern = "1.4.7"
-# winning_pattern = "1.5.9"
-# winning_pattern = "2.5.8"
-# winning_pattern = "3.5.7"
-# winning_pattern = "3.6.9"
-# winning_pattern = "4.5.6"
-# winning_pattern = "7.8.9"
--- app/models/tic_tac_toe_game.rb
+++ app/models/tic_tac_toe_game.rb
@@ -6,20 +6,15 @@ class TicTacToeGame < ApplicationRecord
   def end_of_a_game?
     # anounce that game is finish if there is a winner or if all tile are write
     # announce there is a winner if a winning pattern has all the same symbol
-    winning_patern?
+    # check if there is a winner only if at least 5 tiles are selected
+    return if nil_position >= 5
+
+    # call method result if there is a winning pattern or if nil position is 0
+    winning_patern? || nil_position.zero?
   end
 
   def result
     # should return the result of the game: Name of the winnner or draw
-
-    # if select_winner.flatten.uniq.join == "X"
-    #   cross_player.first_name
-    # elsif select_winner.flatten.uniq.join == "O"
-    #   circle_player.first_name
-    # else
-    #   "draw"
-    # end
-
     case select_winner.flatten.uniq.join
     when "X" then cross_player.first_name
     when "O" then circle_player.first_name
@@ -29,20 +24,25 @@ class TicTacToeGame < ApplicationRecord
 
   private
 
+  def nil_position
+    # method to check if there is still a nill position
+    nil_position = (1..9).map do |n|
+      send("position_#{n}")
+    end
+
+    nil_position.select(&:nil?).size
+  end
+
   def winning_patern?
     # should return true if a winning pattern are all the same symbol
-    # winning_pattern = "1.2.3"
-    # winning_pattern = "1.4.7"
-    # winning_pattern = "1.5.9"
-    # winning_pattern = "2.5.8"
-    # winning_pattern = "3.5.7"
-    # winning_pattern = "3.6.9"
-    # winning_pattern = "4.5.6"
-    # winning_pattern = "7.8.9"
+    # 1| 2 | 3
+    # 4| 5 | 6
+    # 7| 8 | 9
     select_winner.flatten.any?
   end
 
   def select_winner
+    # select the winner combination
     game_pattern.select do |winning_pattern|
       winning_pattern.uniq.size == 1
     end
@@ -50,6 +50,9 @@ class TicTacToeGame < ApplicationRecord
 
   def game_pattern
     # Winning_patterns is an array with winning combination of tic tac toe
+    # 1| 2 | 3
+    # 4| 5 | 6
+    # 7| 8 | 9
     winning_patterns = [[1, 2, 3], [1, 4, 7], [1, 5, 9], [2, 5, 8], [3, 5, 7], [3, 6, 9], [4, 5, 6], [7, 8, 9]]
 
     # I return an array with the pattern of the current game
