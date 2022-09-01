8bba0c624145a357b2f5628aeef8f6bd56df8b75
Alfred Gaudin <gaudin.all@gmail.com>
2022-09-01 12:42:20 +0200

    le timer et la redirection fonctionne

--- app/javascript/controllers/game_matches_controller.js
+++ app/javascript/controllers/game_matches_controller.js
@@ -3,15 +3,19 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="game-matches"
 export default class extends Controller {
   static targets = ["counter"]
+  static values = {
+    url: String
+  }
+
   connect() {
     console.log("heyy")
-    const sessionid = this.element.dataset.sessionid
+    console.log(location)
     setTimeout(() => {  this.counterTarget.innerText = 4; }, 1000)
     setTimeout(() => {  this.counterTarget.innerText = 3; }, 2000)
     setTimeout(() => {  this.counterTarget.innerText = 2; }, 3000)
     setTimeout(() => {  this.counterTarget.innerText = 1; }, 4000)
     setTimeout(() => {  this.counterTarget.innerText = 0; }, 5000)
-    setTimeout(() => {  location.replace(`'/game_matches/${sessionid}/'`); }, 6000);
+    setTimeout(() => {  location.replace(this.urlValue); }, 6000);
   }
 
 }
