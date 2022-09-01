b0d5f696b13061b3b474ae93632a5741f3031f32
benoit brandt <benoit.07@hotmail.com>
2022-09-01 11:08:46 +0200

    correct end in the show of game match

--- app/controllers/game_matches_controller.rb
+++ app/controllers/game_matches_controller.rb
@@ -1,8 +1,8 @@
 class GameMatchesController < ApplicationController
-
   def show
     @game_match = GameMatch.find(params[:id])
     @tic_tac_toe_game = @game_match.matchable
+  end
 
   def counter
   end
