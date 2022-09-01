e628e5a6ea2adf230a50d2bb17fb6569b0078faa
benoit brandt <benoit.07@hotmail.com>
2022-09-01 11:56:20 +0200

    add routes for home page and create link to connection page or trip

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "trip_sessions#new"
+  root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
 
-  resources :trip_sessions, only: %i[show create] do
+  resources :trip_sessions, only: %i[show create new] do
     member do
       patch :join
       patch :leave
