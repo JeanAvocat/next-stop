3fe5795c1facc77763027db3c68e41653ac3aa64
Jean Avocat <jean.avocat@audencia.com>
2022-09-01 14:57:45 +0200

    Messages ok but not live

--- app/controllers/game_matches_controller.rb
+++ app/controllers/game_matches_controller.rb
@@ -2,6 +2,10 @@ class GameMatchesController < ApplicationController
   def show
     @game_match = GameMatch.find(params[:id])
     @tic_tac_toe_game = @game_match.matchable
+
+    # Chatroom
+    @trip_session = @game_match.trip_session
+    @message = Message.new
   end
 
   def counter
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,19 @@
 class MessagesController < ApplicationController
+  def create
+    @game_match = current_user.trip_sessions.last.game_matches.last
+    @message = Message.new(message_params)
+    @message.trip_session_id = current_user.trip_sessions.last.id
+    @message.user_id = current_user.id
+    if @message.save
+      redirect_to game_match_path(@game_match)
+    else
+      render "chatrooms/show", status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content)
+  end
 end
