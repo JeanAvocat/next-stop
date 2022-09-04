26d8a29046f7c588c78a936355ac3ce55364d53e
benoit brandt <benoit.07@hotmail.com>
2022-09-04 17:59:06 +0200

    add logic to remove possibility to play twice in a row and display who have to play the next round

--- app/controllers/tic_tac_toe_games_controller.rb
+++ app/controllers/tic_tac_toe_games_controller.rb
@@ -2,14 +2,25 @@ class TicTacToeGamesController < ApplicationController
   def select_tile
     @game_match = GameMatch.find(params[:id])
     @tic_tac_toe_game = @game_match.matchable
-    if @tic_tac_toe_game.cross_player == current_user
+    broadcast if update_tile
+    @game_match.update(winner: @tic_tac_toe_game.result.split.last) if @tic_tac_toe_game.end_of_a_game?
+  end
+
+  private
+
+  def update_tile
+    # stop the action if end of game
+    return if @tic_tac_toe_game.end_of_a_game?
+
+    # methode to define who can play, update of the tic tac toe table only if it is to the current player to play
+    if [@tic_tac_toe_game.cross_player, @tic_tac_toe_game.who_have_to_play, current_user].uniq.count == 1
       @tic_tac_toe_game.update("position_#{params[:tile]}": "cross")
-    else
+    elsif [@tic_tac_toe_game.circle_player, @tic_tac_toe_game.who_have_to_play, current_user].uniq.count == 1
       @tic_tac_toe_game.update("position_#{params[:tile]}": "circle")
     end
+  end
 
-    @tic_tac_toe_game.who_have_to_play
-
+  def broadcast
     TicTacToeGameChannel.broadcast_to(
       @game_match,
       render_to_string(partial: "tic_tac_toe_games/selected_tile", locals: { tic_tac_toe_game: @tic_tac_toe_game, n: params[:tile] })
--- app/javascript/controllers/tic_tac_toe_game_subscription_controller.js
+++ app/javascript/controllers/tic_tac_toe_game_subscription_controller.js
@@ -3,35 +3,45 @@ import { createConsumer } from "@rails/actioncable"
 
 // Connects to data-controller="tic-tac-toe-game-subscription"
 export default class extends Controller {
-  // static targets = ["tile1", "tile2", "tile3", "tile4", "tile5", "tile6", "tile7", "tile8", "tile9"]
-  static values = { gameId: Number }
-  static targets = ["tile"]
+  static values = { gameId: Number, result: Boolean }
+  static targets = ["tile", "nickname", "competitor", "result"]
 
   connect() {
     this.channel = createConsumer().subscriptions.create(
       { channel: "TicTacToeGameChannel", id: this.gameIdValue },
       {  received: data => this.#updateTile(data) }
     )
-    console.log(`Subscribe to the tic tac toe game with the id ${this.gameIdValue}.`)
+    // console.log(`Subscribe to the tic tac toe game with the id ${this.gameIdValue}.`)
     // console.log("hello tic tac toe");
-    // console.log(this.tictactoeTarget)
+    // console.log(this.nicknameTarget.innerText)
   }
 
 
   #updateTile(data) {
-    // event.preventDefault()
-    // console.log(this.tictactoeTarget.innerHTML);
-    console.log(data);
-    // console.log(this.tileTargets)
+    // methode to update tile on both screen
     this.tileTargets.forEach(tile => {
       if (data.includes(tile.id)) {
-        console.log(tile);
-        tile.innerHTML = data
+        tile.outerHTML = data;
+        this.#updatePlayer();
+        this.#endOfGame(data);
       }
     });
-    // this.tictactoeTarget.innerHTML = data
-    // this.tileTargets.forEach(target => {
-    // console.log(target);
-    // })
+  }
+
+  #updatePlayer() {
+    // methode to update info about player who have to play
+    if (this.nicknameTarget.innerText === "C'est à ton tour de jouer") {
+      this.nicknameTarget.innerText = `C'est au tour de ${this.competitorTarget.innerText}`;
+    } else {
+      this.nicknameTarget.innerText = "C'est à ton tour de jouer";
+    }
+  }
+
+  #endOfGame(data) {
+    if ((data.includes("égalité")) || (data.includes("gagnant"))) {
+      this.finalResult = data.split(`"`).filter(element => element.includes("gagnant") || element.includes("égalité")).join()
+      this.resultTarget.innerHTML = `<h6> ${this.finalResult} </h6>`;
+      // alert("la partie est terminée")
+    }
   }
 }
