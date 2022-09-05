56b1866f61573c3af416fd5d3a71b4792bb582fa
benoit brandt <benoit.07@hotmail.com>
2022-09-05 18:02:30 +0200

    refacto code to replay a game

--- app/controllers/game_matches_controller.rb
+++ app/controllers/game_matches_controller.rb
@@ -11,25 +11,20 @@ class GameMatchesController < ApplicationController
   end
 
   def create
-    @previous_game_match = GameMatch.find(params[:previous_game_match])
+    # find the previous game match of both players together before to save the next one
+    @previous_game_match = find_last_trip_sessions.game_matches.last
+    # create the new tic tac toe game
+    tic_tac_toe_game = tic_tac_toe_creation
     # creation of new instance of game_match and tic tac toe game
-    @tic_tac_toe_game = TicTacToeGame.new
     @game_match = GameMatch.new
-    # find the last trip_sessions id from the last game_match
-    @game_match.trip_session_id = @previous_game_match.trip_session_id
-    # assign cross player and circle player to new tic tac toe game
-    @tic_tac_toe_game.cross_player = @game_match.trip_session.joiner
-    @tic_tac_toe_game.circle_player = @game_match.trip_session.creator
-    # save the new tic tac toe game instance in DB
-    @tic_tac_toe_game.save
+    # find the last trip_sessions from the last trip_sessions of current user
+    @game_match.trip_session = find_last_trip_sessions
     # add the tic tac toe game id to the game match
-    @game_match.matchable = @tic_tac_toe_game
+    @game_match.matchable = tic_tac_toe_game
     # save the new game_match instance in DB
     @game_match.save
-    TicTacToeGameChannel.broadcast_to(
-      @previous_game_match,
-      @previous_game_match.id
-    )
+    # send info to other player to restart the game
+    broadcast_previous_game_match
     # redirect to game_match show
     redirect_to game_match_path(@game_match)
   end
@@ -47,6 +42,24 @@ class GameMatchesController < ApplicationController
     # define last trip_sessions_as_joiner
     joiner_id = current_user.trip_sessions_as_joiner.last.id
     # return the bigger id concerning the trip sessions
-    creator_id > joiner_id ? creator_id : joiner_id
+    creator_id > joiner_id ? TripSession.find(creator_id) : TripSession.find(joiner_id)
+  end
+
+  def tic_tac_toe_creation
+    # assign cross player and circle player to new tic tac toe game
+    tic_tac_toe_game = TicTacToeGame.new
+    tic_tac_toe_game.cross_player = find_last_trip_sessions.joiner
+    tic_tac_toe_game.circle_player = find_last_trip_sessions.creator
+    # save the new tic tac toe game instance in DB
+    tic_tac_toe_game.save
+    # return tic tac toe game
+    tic_tac_toe_game
+  end
+
+  def broadcast_previous_game_match
+    TicTacToeGameChannel.broadcast_to(
+      @previous_game_match,
+      @previous_game_match.id
+    )
   end
 end
