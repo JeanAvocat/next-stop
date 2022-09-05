cf0d388881c3a936d6056846b08810a1d0319050
Alfred Gaudin <gaudin.all@gmail.com>
2022-09-05 14:59:36 +0200

    la redirection qui marche, plus de pub et le changement de message

--- app/javascript/controllers/joiner_listener_controller.js
+++ app/javascript/controllers/joiner_listener_controller.js
@@ -3,25 +3,27 @@ import { createConsumer } from "@rails/actioncable"
 
 // Connects to data-controller="game-matches"
 export default class extends Controller {
+  static targets = [ "joinmessage"]
   static values = {
     status: String,
     url: String,
     sessionid: Number
 
   }
-
   connect() {
-  console.log(`hey ${this.sessionidValue}`);
-  console.log(`hey ${this.statusValue}`);
-    // // if (this.statusValue === "in game") {
-    // //   location.replace(this.urlValue)
-    // }
     this.channel = createConsumer().subscriptions.create(
       { channel: "ListenJoinerChannel", id: this.sessionidValue },
-      {  received: data => console.log(data)  }
+      {  received: data => this.#updateTile(data)  }
+
      )
   }
-  
+  #updateTile(data) {
+    console.log(this.joinmessageTarget)
+    this.joinmessageTarget.innerText = "Vous allez rejoindre votre binôme de jeu"
+    setTimeout(() => {  location.replace(`/game_matches/${data}/counter`) }, 3000)
+
+  }
+
 
 
 }
