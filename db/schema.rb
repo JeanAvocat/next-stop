3d6e47159185ea52d0e352dc7d4fb7f21fd65ba6
Jean Avocat <jean.avocat@audencia.com>
2022-09-07 19:45:43 +0200

    add 0 as default value for Score field

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_07_170557) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_07_173401) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -125,7 +125,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_07_170557) do
     t.datetime "updated_at", null: false
     t.string "first_name"
     t.string "random_nickname"
-    t.integer "score"
+    t.integer "score", default: 0
     t.string "phone_number"
     t.string "instagram_account"
     t.index ["email"], name: "index_users_on_email", unique: true
