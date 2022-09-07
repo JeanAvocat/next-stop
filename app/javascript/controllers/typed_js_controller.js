4589c08b34f1639e72a321a3eaf80b0c85abaa6d
Marguerite Choyer <margueritechoyer@gmail.com>
2022-09-07 09:56:47 +0200

    Pas final

--- app/javascript/controllers/typed_js_controller.js
+++ app/javascript/controllers/typed_js_controller.js
@@ -1,7 +1,15 @@
 import { Controller } from "@hotwired/stimulus"
+import Typed from "typed.js"
 
 // Connects to data-controller="typed-js"
 export default class extends Controller {
   connect() {
+    console.log("hello world");
+    new Typed(this.element, {
+      strings: ['Chaque bonne journée commence par un bon trajet : sociabilisez facilement dans les transports en commun'],
+      showCursor: false,
+      typeSpeed: 1,
+      loop: false
+    })
   }
 }
