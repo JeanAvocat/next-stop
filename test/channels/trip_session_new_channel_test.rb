64527953deda75e0f54257c99a664e6764a93281
Alfred Gaudin <gaudin.all@gmail.com>
2022-09-07 13:03:57 +0200

    le nickname aléatoire

---
+++ test/channels/trip_session_new_channel_test.rb
@@ -0,0 +1,8 @@
+require "test_helper"
+
+class TripSessionNewChannelTest < ActionCable::Channel::TestCase
+  # test "subscribes" do
+  #   subscribe
+  #   assert subscription.confirmed?
+  # end
+end
