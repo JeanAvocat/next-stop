3f9100016d5840facacbfe8889433a270493119a
Jean Avocat <jean.avocat@audencia.com>
2022-09-07 13:15:30 +0200

    basic methods ok

--- app/models/chifoumi_game.rb
+++ app/models/chifoumi_game.rb
@@ -9,24 +9,34 @@ class ChifoumiGame < ApplicationRecord
   # check if there is a winner only if at least 5 tiles are selected
   return if play_round >= 4
   # return true if there is a winning pattern
-  final_winning_patern?
+  final_winner?
   end
 
   def result
     # should return the result of the game: Name of the winnner
-    case final_winning_patern?
+    case final_winner?
     when "first_player" then "Le gagnant est #{first_player.random_nickname}"
     when "second_player" then "Le gagnant est #{second_player.random_nickname}"
+    when "draw" then "Pas de gagnant, le match continue !"
     end
   end
 
-  def round_patern?
-    # should return true if a winning combination is present
-    
-    winning_combinations = ["rock" > "scissors", "scissors" > "paper", "paper" > "rock"]
+  def round_paterns?
+
+    # winning_combinations = ["rock" > "scissors", "scissors" > "paper", "paper" > "rock"]
+
+    if (first_player_choice == "rock" && second_player_choice == "scissors") || (first_player_choice == "scissors" && second_player_choice == "paper") || (first_player_choice == "paper" && second_player_choice == "rock")
+      first_player_score += 1
+      play_round += 1
+    elsif (second_player_choice == "rock" && first_player_choice == "scissors") || (second_player_choice == "scissors" && first_player_choice == "paper") || (second_player_choice == "paper" && first_player_choice == "rock")
+      second_player_score += 1
+      play_round += 1
+    elsif first_player_choice == second_player_choice
+      play_round += 1
+    end
   end
 
-  def final_winning_patern?
+  def final_winner?
     # should return true if there is a final winner
     (first_player_score - second_player_score).abs >= 2 && (first_player_score >= 3 || second_player_score >= 3)
   end
