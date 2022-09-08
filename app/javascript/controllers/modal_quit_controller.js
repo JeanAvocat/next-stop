21ac05a58f7ca2bf4045210f095beca92db6c38a
Jean Avocat <jean.avocat@audencia.com>
2022-09-08 13:54:47 +0200

    fix alert js controller when buddy quit

--- app/javascript/controllers/modal_quit_controller.js
+++ app/javascript/controllers/modal_quit_controller.js
@@ -5,11 +5,11 @@ export default class extends Controller {
 
   static targets = ["modal", "modalQuitContainer"]
   connect() {
-    console.log("connected")
+    // console.log("connected")
   }
 
   displayModal(event) {
-    console.log(this.fullContentTarget)
+    // console.log(this.fullContentTarget)
     event.preventDefault()
     this.modalTarget.classList.toggle("d-none")
     this.modalQuitContainerTarget.classList.toggle("d-none")
