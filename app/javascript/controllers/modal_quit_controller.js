67a6a93b1faf1ec69161452128094d5d54cf52cf
Jean Avocat <jean.avocat@audencia.com>
2022-09-08 12:34:36 +0200

    message great with opacity

--- app/javascript/controllers/modal_quit_controller.js
+++ app/javascript/controllers/modal_quit_controller.js
@@ -3,16 +3,15 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="modal-quit"
 export default class extends Controller {
 
-  static targets = ["modal", "arrow", "modalQuitContainer"]
+  static targets = ["modal", "modalQuitContainer"]
   connect() {
     console.log("connected")
   }
 
   displayModal(event) {
-    console.log("Coucou")
+    console.log(this.fullContentTarget)
     event.preventDefault()
     this.modalTarget.classList.toggle("d-none")
     this.modalQuitContainerTarget.classList.toggle("d-none")
-    // this.arrowTarget.classList.toggle("d-none")
   }
 }
