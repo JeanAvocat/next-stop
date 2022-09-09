ed7f4db8fd2d6bb9453d5a0d109a0473ce4e334d
Marguerite Choyer <margueritechoyer@gmail.com>
2022-09-09 09:53:13 +0200

    Modification vitesse

--- app/javascript/controllers/typed_js_controller.js
+++ app/javascript/controllers/typed_js_controller.js
@@ -8,7 +8,7 @@ export default class extends Controller {
     new Typed(this.element, {
       strings: ['Chaque bonne journée commence par un bon trajet : sociabilisez facilement dans les transports en commun'],
       showCursor: false,
-      typeSpeed: 0,
+      typeSpeed: 30,
       loop: false,
       startDelay: 10
     })
